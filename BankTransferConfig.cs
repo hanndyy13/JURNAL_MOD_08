ab93fb6b1f8936849c159f73254d9e75e28e5c7a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:18:59 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static modul8_103022300064.BankTransferConfig;
+
+namespace modul8_103022300064
+{
+
+    public class BankTransferConfig
+    {
+        public string? lang { get; set; }
+        public string[] method { get; set; }
+
+        public class Transfer
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+
+        public class Confirmation
+        {
+            public string end { get; set; }
+            public string id { get; set; }
+        }
+
+        const string fileConfig = "D:\tpmod08\tpmod08\bank_transfer_config.json";
+
+        public static BankTransferConfig LoadConfig()
+        {
+            if (File.Exists(fileConfig))
+            {
+                string jsonString = File.ReadAllText(fileConfig);
+                BankTransferConfig config = JsonSerializer.Deserialize<BankTransferConfig>(jsonString);
+
+                if (config != null)
+                    return config;
+            }
+
+            BankTransferConfig bankTransferConfig = new BankTransferConfig
+            {
+                lang = "en",
+                Transfer = new BankTransferConfig.Transfer
+                {
+                    threshold = 2500000,
+                    low_fee = 6500,
+                    high_fee = 15000
+                },
+                method = ["“RTO", "(real-time)”", "“SKN”", "“RTGS”", "“BI", "FAST”"],
+                Confirmation = new BankTransferConfig.Confirmation
+                {
+                     end = "yes",
+                     id = "ya"
+                }
+
+            };
+
+            string defaultJson = JsonSerializer.Serialize(bankTransferConfig, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(fileConfig, defaultJson);
+
+            return bankTransferConfig;
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul8_103022300064;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig bankTransferConfig = new BankTransferConfig();
+        int input;
+        int amountTransfer;
+        Console.WriteLine("Pilih Bahasa (English atau Indonesia) = ");
+        bankTransferConfig.lang = Console.ReadLine();
+
+        if (bankTransferConfig.lang.Equals("en"))
+        {
+            Console.WriteLine("Please insert the amount of money to transfer =  ");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input <= bankTransferConfig.threshold)
+            {
+                amountTransfer = bankTransferConfig.low_fee;
+                Console.WriteLine($"Transfer fee = {amountTransfer} dan Total amount = {input + amountTransfer}");
+
+            }
+            else
+            {
+                amountTransfer = bankTransferConfig.high_fee;
+                Console.WriteLine($"Transfer fee = {amountTransfer} dan Total amount = {input + amountTransfer}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer =  ");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input <= bankTransferConfig.threshold)
+            {
+                amountTransfer = bankTransferConfig.low_fee;
+                Console.WriteLine($"Transfer fee = {amountTransfer} dan Total amount = {input + amountTransfer}");
+
+            }
+            else
+            {
+                amountTransfer = bankTransferConfig.high_fee;
+                Console.WriteLine($"Transfer fee = {amountTransfer} dan Total amount = {input + amountTransfer}");
+            }
+        }
+        
+
+
+
+        Console.WriteLine("\nTekan sembarang tombol untuk keluar...");
+        Console.ReadKey();
+    }
+}
